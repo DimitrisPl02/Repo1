1edaf578916e226131f7bc840dc52b3dcb447076
DimitrisPl02 <73967723+DimitrisPl02@users.noreply.github.com>
2020-11-06 21:11:35 +0200

    Add files via upload

---
+++ Temperature.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	float T1,T2;
+	cout <<"insert value of temperature in Celsious is: ";
+	cin  >>T1;
+	T2=((9*T1)/5)+32 ;
+	cout <<"the same temperature in Fahrenheit is: "<< T2 <<endl;
+}
---
+++ Volume.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main ()
+{
+    float A,V;
+    cout <<"if the edge of a cube is ";
+    cin  >>A;
+    V=A*A*A;
+    cout <<"then the volume of this cube is "<<V << endl; 
+}
